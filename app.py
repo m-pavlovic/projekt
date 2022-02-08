e0dbc53ff191ee7c1f6c69610bbcc287401bf09d
Marija Pavlovic <marija.pavlovic19@gmail.com>
2022-02-08 19:50:29 +0100

    blog run with flask

--- app.py
+++ app.py
@@ -6,29 +6,5 @@ app = Flask(__name__)
 def index():
     return render_template('blog.html')
 
-@app.route('/faq')
-def index():
-    return render_template('faq.html')
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/kontakt')
-def index():
-    return render_template('kontakt.html')
-
-@app.route('/linkovi')
-def index():
-    return render_template('linkovi.html')
-
-@app.route('/obrazac')
-def index():
-    return render_template('obrazac.html')
-
-@app.route('/opgovi')
-def index():
-    return render_template('opgovi.html')
-
 if __name__ == '__main__':
     app.run(debug=True)
