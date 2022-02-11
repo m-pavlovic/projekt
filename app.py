74a09aaa2e4a0d2242a11f1c490feb7506b41ebd
Marija Pavlovic <marija.pavlovic19@gmail.com>
2022-02-11 18:25:17 +0100

    obrazac u wtforms

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
+
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, IntegerField, BooleanField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "blabla"
+
+
+class OPGForm(FlaskForm):
+    opg = StringField("Ime OPG-a", validators=[DataRequired()])
+    ime = StringField("Ime i prezime vlasnika", validators=[DataRequired()])
+    oib = IntegerField("OIB", validators=[DataRequired()])
+    kontakt = IntegerField("Kontakt broj", validators=[DataRequired()])
+    accept_tos = BooleanField("Sve su tvrdnje točne i u skladu sa pravilnikom ZdPolja.", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 @app.route('/')
 def index():
@@ -22,13 +36,21 @@ def kontakt():
 def linkovi():
     return render_template('linkovi.html')
 
-@app.route('/obrazac')
+@app.route('/obrazac', methods=['GET', 'POST'])
 def obrazac():
-    return render_template('obrazac.html')
+    form = OPGForm()
+    if form.validate_on_submit():
+        return render_template('uspjeh.html')
+    return render_template('obrazac.html',
+    form = form)
+
+
 
 @app.route('/opgovi')
 def opgovi():
     return render_template('opgovi.html')
+
+
 
 
 
